82ac00ab44a70be4e1076b32405eb9c88b4c792a
Viktor Farcic <viktor@farcic.com>
2022-01-21 03:20:49 +0100

    SQL

---
+++ apps-code/sql/README.md
@@ -0,0 +1,9 @@
+```bash
+docker image build --tag vfarcic/sql-demo .
+
+docker image tag vfarcic/sql-demo vfarcic/sql-demo:0.1.1
+
+docker image push vfarcic/sql-demo
+
+docker image push vfarcic/sql-demo:0.1.1
+```
--- apps-code/sql/main.go
+++ apps-code/sql/main.go
@@ -3,25 +3,47 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("Go MySQL Tutorial")
-	// Change to env vars
-	db, err := sql.Open("mysql", "root:yVUP9OXMC7GqCksrE872UNffftw@tcp(34.73.107.106:3306)/dot?tls=false")
+	http.HandleFunc("/", root)
+	http.HandleFunc("/addVideo", addVideo)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	log.Println("Starting the server on " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func root(w http.ResponseWriter, req *http.Request) {
+	log.Println("Not yet implemented...")
+}
+
+func addVideo(w http.ResponseWriter, req *http.Request) {
+	log.Println("Adding a video...")
+	db, err := sql.Open("mysql", "root:OSAldwySWcXNxxUCCMlXlXUjU05@tcp(35.229.106.29:3306)/sql-demo?tls=false")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
-	fmt.Println("111")
-	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
-	fmt.Println("222")
+	id := req.URL.Query().Get("id")
+	title := req.URL.Query().Get("title")
+	url := req.URL.Query().Get("url")
+	query := fmt.Sprintf(
+		"INSERT INTO videos VALUES ('%s', '%s', '%s')",
+		id,
+		title,
+		url,
+	)
+	insert, err := db.Query(query)
 	if err != nil {
-		fmt.Println("333")
 		panic(err.Error())
 	}
-	fmt.Println("444")
 	defer insert.Close()
 }
