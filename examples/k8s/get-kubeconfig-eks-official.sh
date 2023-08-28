099591a42ff45e7ab8deec9edab2fd379c5f70c9
Viktor Farcic <viktor@farcic.com>
2023-08-28 17:33:21 +0200

    AWS

---
+++ examples/k8s/get-kubeconfig-eks-official.sh
@@ -0,0 +1,8 @@
+export NAMESPACE=${1:-a-team}
+
+export SECRET=${2:-a-team-eks-cluster}
+
+kubectl --namespace $NAMESPACE \
+    get secret $SECRET \
+    --output jsonpath="{.data.kubeconfig}" \
+    | base64 -d | tee kubeconfig.yaml
